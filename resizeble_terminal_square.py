217bc853e319396a1eb20b24bf97b4f67bbd32a4
floordiv <pavloromanenko25@gmail.com>
2020-04-19 04:32:15 +0300

    newline problem not fixed, added colors (termcolor), removed threading

--- resizeble_terminal_square.py
+++ resizeble_terminal_square.py
@@ -1,32 +1,25 @@
 import os
 from time import sleep
 from platform import system
-from threading import Thread
+from termcolor import colored
 
 
-class data:
-    resolution = [0, 0]
-    frametime = 1 / 20  # 20 fps
-    cls = 'cls' if system().lower() == 'windows' else 'clear'   # cross-platform
-
-
-def terminal_size_listener():
-    while True:
-        data.resolution = os.get_terminal_size()
-        sleep(data.frametime)
+frametime = 1 / 20  # 20 fps
+cls = 'cls' if system().lower() == 'windows' else 'clear'   # cross-platform
+frame_color = 'red'
+bgcolor = 'blue'
 
 
 def square_draw():
-    sleep(0.1)
     while True:
-        os.system(data.cls)
-        w, h = data.resolution
-        print('-' * w)
+        w, h = os.get_terminal_size()
+        os.system(cls)
+        print(colored('-' * w, frame_color, 'on_' + bgcolor))
         for _ in range(h - 3):
-            print('|' + ' ' * (w - 2) + '|')
-        print('-' * w)
-        sleep(data.frametime)
+            print(colored('|' + ' ' * (w - 2) + '|', frame_color, 'on_' + bgcolor))
+        print(colored('-' * w, frame_color, 'on_' + bgcolor))
+        sleep(frametime)
 
 
 if __name__ == '__main__':
-    Thread(target=terminal_size_listener).start(), Thread(target=square_draw).start()
+    square_draw()
