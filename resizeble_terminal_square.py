1e57196dd75a38ebd1efd70ba017ba032f87ffc9
floordiv <pavloromanenko25@gmail.com>
2020-04-19 04:43:34 +0300

    added arguments

--- resizeble_terminal_square.py
+++ resizeble_terminal_square.py
@@ -1,10 +1,11 @@
 import os
+from sys import argv
 from time import sleep
 from platform import system
 from termcolor import colored
 
 
-frametime = 1 / 20  # 20 fps
+frametime = 20  # 20 fps
 cls = 'cls' if system().lower() == 'windows' else 'clear'   # cross-platform
 frame_color = 'red'
 bgcolor = 'blue'
@@ -22,4 +23,13 @@ def square_draw():
 
 
 if __name__ == '__main__':
+    if len(argv) > 1:   # we have some given arguments
+        args = argv[1:]
+
+        for arg, var in [('--frame-color', 'frame_color'),
+                         ('--bg-color', 'bgcolor'),
+                         ('--fps', 'frametime')]:
+            if arg in args:
+                vars()[var] = args[args.index(arg) + 1]
+    frametime = 1 / int(frametime)  # fps counter will be shown as a string if passed threw an argument
     square_draw()
